cac795d7636886af387fed4e8bfb1f956327e590
Anton <94841246+ImScarecrow5@users.noreply.github.com>
2025-10-26 20:46:53 +0300

    312

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using ConsoleApp1;
+using System;
+using System.Threading.Tasks;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        task4();
+        Console.WriteLine("TEST");
+    }
+    static void task1()
+    {
+        Console.WriteLine("Вводите числа:\n");
+        int numInput;
+        int evenCount = 0;
+        while (int.TryParse(Console.ReadLine(), out numInput))
+        {
+            if (numInput % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        Console.WriteLine(evenCount);
+    }
+
+    static void task2()
+    {
+        int K;
+        try
+        {
+            K = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= K; i++)
+            {
+                if (i % 10 == 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        catch { Console.WriteLine("Введено не число!"); }
+        ;
+    }
+
+    static void task3()
+    {
+        int count = 0;
+        try
+        {
+            while (Convert.ToDouble(Console.ReadLine()) != 0)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+        catch { Console.WriteLine("Введено не число!"); }
+    }
+
+    static void task4()
+    {
+        Form1 task4 = new Form1();
+        task4.Show();
+    }
+}
